c2fba4c762da9347185c2dc9faaf4dd7676c4256
Arth <arth4701@gmail.com>
2022-02-12 22:30:33 +0530

    Insertion sort added

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -1,4 +1,27 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        int l = arr.length;
+        for(int i = 0; i < l - 1; i++)
+        {
+            for(int j = i + 1; j > 0; j--)
+            {
+                if(arr[j] < arr[j-1])
+                {
+                    int tmp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = tmp;
+                }
+               else{
+                   break;
+                }
+            }
+        System.out.println(Arrays.toString(arr));
+        }
+
+    }
 }
