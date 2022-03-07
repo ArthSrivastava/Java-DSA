216cd98dfaa7f29d1fe6e98daf3ea7a73c156651
Arth <arth4701@gmail.com>
2022-03-08 00:23:03 +0530

    Added some operations in binary tree

--- src/Tree/Main.java
+++ src/Tree/Main.java
@@ -4,11 +4,13 @@ public class Main {
     public static void main(String[] args) {
         BinaryTreeNode root = new BinaryTreeNode(40);
         root.left = new BinaryTreeNode(10);
+        root.left.right = new BinaryTreeNode(70);
         root.right = new BinaryTreeNode(20);
         root.right.left = new BinaryTreeNode(76);
         root.right.left.right = new BinaryTreeNode(87);
         root.left.left = new BinaryTreeNode(100);
         root.right.right = new BinaryTreeNode(50);
+        root.left.left.left = new BinaryTreeNode(1000);
         OperationsOnBinaryTree operations = new OperationsOnBinaryTree();
         System.out.print("InOrder:");
         operations.inOrderTraversal(root);
@@ -19,14 +21,25 @@ public class Main {
         System.out.print("PostOrder:");
         operations.postOrderTraversal(root);
         System.out.println();
-        System.out.print("Height:");
-        System.out.println(operations.height(root));
+//        System.out.print("Height:");
+//        System.out.println(operations.height(root));
 
         //bfs traversal O(h * n);
-        int ht = operations.height(root);
-        for(int i = 1; i <= ht; i++)
-        {
-            operations.bfsTraversal(root, i);
-        }
+//        int ht = operations.height(root);
+//        for(int i = 1; i <= ht; i++)
+//        {
+//            operations.bfsTraversal(root, i);
+//        }
+//        System.out.println();
+//        operations.bfsLineByLine(root);
+//
+//        System.out.println();
+//
+//        operations.bfsLineByLine2(root);
+//        operations.bfsOptimised(root);
+
+        System.out.println("Size:" + operations.getSize(root));
+        operations.bfsOptimised(root);
+        System.out.println(operations.getMax(root));
     }
 }
--- src/Tree/OperationsOnBinaryTree.java
+++ src/Tree/OperationsOnBinaryTree.java
@@ -3,6 +3,9 @@ package Tree;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class OperationsOnBinaryTree {
     public void inOrderTraversal(BinaryTreeNode root){
         if(root == null)
@@ -29,6 +32,7 @@ public class OperationsOnBinaryTree {
         }
         postOrderTraversal(root.left);
         postOrderTraversal(root.right);
+
         System.out.print(" " + root.data + " ");
     }
 
@@ -51,6 +55,89 @@ public class OperationsOnBinaryTree {
         }
         bfsTraversal(root.left, k - 1);
         bfsTraversal(root.right, k - 1);
+    }
+
+
+    public void bfsOptimised(BinaryTreeNode root)
+    {
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        BinaryTreeNode curr;
+        int count = 1;
+        int max = Integer.MIN_VALUE;
+        while(!queue.isEmpty())
+        {
+            curr = queue.poll();
+            max = Math.max(curr.data, max);
+            System.out.print(curr.data + " ");
+            if(curr.left != null) {
+                queue.add(curr.left);
+                count++;
+            }
+            if (curr.right != null) {
+                queue.add(curr.right);
+                count++;
+            }
+        }
+        System.out.println();
+//        System.out.println("Size:" + count);
+        System.out.println("Max Element:" + max);
+        System.out.println();
+    }
+
+    public void bfsLineByLine(BinaryTreeNode root)
+    {
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+        BinaryTreeNode curr;
+        while(queue.size() > 1)
+        {
+            curr = queue.poll();
+            if(curr == null)
+            {
+                System.out.println();
+                queue.add(null);
+            }
+            else {
+                System.out.print(curr.data + " ");
+                if (curr.left != null)
+                    queue.add(curr.left);
+                if (curr.right != null)
+                    queue.add(curr.right);
+            }
+        }
+    }
+
+    public void bfsLineByLine2(BinaryTreeNode root) {
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        BinaryTreeNode curr;
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                curr = queue.poll();
+                System.out.print(curr.data + " ");
+                if (curr.left != null)
+                    queue.add(curr.left);
+                if (curr.right != null)
+                    queue.add(curr.right);
+            }
+            System.out.println();
+        }
+    }
+
+    public int getSize(BinaryTreeNode root)
+    {
+        if(root == null)
+            return 0;
+        return 1 + getSize(root.left) + getSize(root.right);
+    }
+
+    public int getMax(BinaryTreeNode root){
+        if(root == null)
+            return Integer.MIN_VALUE;
+        return Math.max(root.data, Math.max(getMax(root.left), getMax(root.right)));
     }
 
 }
