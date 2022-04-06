edef09a5b0bd3c09961c7b2546c0893fe07d82f5
Arth <arth4701@gmail.com>
2022-04-06 19:24:41 +0530

    DetectCycleUsingKahn BFS based algo added in directed graph cycle detection

--- src/Graph/DetectCycleInADirectedGraph.java
+++ src/Graph/DetectCycleInADirectedGraph.java
@@ -1,6 +1,8 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class DetectCycleInADirectedGraph {
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
@@ -32,5 +34,38 @@ public class DetectCycleInADirectedGraph {
         }
         recSt[s] = false;
         return false;
+    }
+
+    public boolean detectCycleUsingKahn(int V, ArrayList<ArrayList<Integer>> adj) {
+        int[] inDeg = new int[V];
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < adj.size(); i++) {
+            for(int j = 0; j < adj.get(i).size(); j++)
+            {
+                inDeg[adj.get(i).get(j)]++;
+            }
+        }
+        // for(int i = 0; i < inDeg.length; i++)
+        // {
+        //     System.out.println(i + ": " + inDeg[i]);
+        // }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < inDeg.length; i++) {
+            if(inDeg[i] == 0)
+                queue.add(i);
+        }
+        int count = 0;
+        while(!queue.isEmpty()) {
+            int u = queue.poll();
+            list.add(u);
+            for(int v: adj.get(u)) {
+                // if(inDeg[v] != 0)
+                inDeg[v] -= 1;
+                if(inDeg[v] == 0)
+                    queue.add(v);
+            }
+            count++;
+        }
+        return count != V;
     }
 }
