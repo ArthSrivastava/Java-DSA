13e32d787c15a1cde2271c91743282db89551304
Arth <arth4701@gmail.com>
2022-05-04 22:01:11 +0530

    PreviousGreaterElement modified: added a new method

--- src/Stack/PracticeProblems/PreviousGreaterElement.java
+++ src/Stack/PracticeProblems/PreviousGreaterElement.java
@@ -1,5 +1,6 @@
 package Stack.PracticeProblems;
 
+import java.util.ArrayList;
 import java.util.Stack;
 import java.util.Arrays;
 public class PreviousGreaterElement {
@@ -19,8 +20,33 @@ public class PreviousGreaterElement {
         System.out.println(Arrays.toString(prevGreater));
     }
 
+    private static void nearestGreaterToLeft(int[] arr) {
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (stack.isEmpty()) {
+                list.add(-1);
+            } else if (!stack.isEmpty() && stack.peek() > arr[i]) {
+                list.add(stack.peek());
+            } else if (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                    stack.pop();
+                }
+                if (stack.isEmpty()) {
+                    list.add(-1);
+                } else {
+                    list.add(stack.peek());
+                }
+            }
+            stack.push(arr[i]);
+        }
+        System.out.println(list);
+    }
+
     public static void main(String[] args) {
         int[] arr = {40, 30, 20, 10};
         getPrevGreaterEle(arr);
+        nearestGreaterToLeft(arr);
     }
 }
